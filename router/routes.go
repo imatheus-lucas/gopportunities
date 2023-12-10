384c0d0869c4bfa80dd0ecc73aa8da19f00ed433
imatheus-lucas <matheeuslucas13@gmail.com>
2023-12-10 18:08:55 -0300

    feat: :sparkles: add separete files for route handlers

--- router/routes.go
+++ router/routes.go
@@ -1,33 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/imatheus-lucas/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opportunities", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Server is running",
-			})
-		})
-		v1.POST("/opportunities", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Server is running",
-			})
-		})
-		v1.DELETE("/opportunities", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Server is running",
-			})
-		})
-		v1.PUT("/opportunities", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Server is running",
-			})
-		})
+		v1.GET("/opportunity", handler.ShowOpeningHandler)
+		v1.POST("/opportunities", handler.CreateOpeningHandler)
+		v1.DELETE("/opportunities", handler.DeleteOpeningHandler)
+		v1.PUT("/opportunities", handler.UpdateOpeningHandler)
+		v1.GET("/opportunities", handler.ListOpeningHandler)
 	}
 }
